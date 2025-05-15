02a9325757efe46ac46bb8b27fb7315f84471635
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-05-15 23:15:28 +0300

    initial app.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
   optimizeDeps: {
-    include: ['tailwindcss'],
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
 })
