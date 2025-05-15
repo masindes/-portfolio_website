aba6fb8fa2d032656da079b385cb587c4845b83b
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-05-15 22:54:15 +0300

    initial style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust for your framework
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        secondary: "#3b82f6",
+        dark: "#0f172a",
+      },
+      animation: {
+        "spin-slow": "spin 8s linear infinite",
+      }
+    },
   },
   plugins: [],
+  darkMode: "class",
 }
